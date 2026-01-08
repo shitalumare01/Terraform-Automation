f2f095e791a0f0dc04847c46e0563e7b03354a6e
Shital <shitalumare06@gmail.com>
2026-01-08 20:01:26 +0530

    add code

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "ap-south-1" 
+       default     = "us-east-1" 
 }
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "jenkins-ap-south-1" 
+    default     =  "jenkins-server" 
 }
 
 variable "instance_type" { 
     description = "instance type for ec2" 
-    default     =  "t2.micro" 
+    default     =  "t2.medium" 
 }
 
 variable "security_group" { 
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-07ffb2f4d65357b42" 
+    default     = "ami-0ecb62995f68bb549" 
 }
 variable "versioning" {
     type        = bool
